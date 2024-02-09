37ab0cd4183463643434c523300a45aa18f32678
aknochow <aknochow@redhat.com>
2024-02-09 14:25:29 -0500

    updating /api/galaxy/v3/plugin/execution-environments/repositories/ url in ci

--- .ci/scripts/galaxy_ng-tests.sh
+++ .ci/scripts/galaxy_ng-tests.sh
@@ -57,8 +57,7 @@ podman login --tls-verify=false -u admin -p password localhost:24880
 podman tag quay.io/ansible/galaxy-operator:main localhost:24880/ansible/galaxy-operator:main
 podman push --tls-verify=false localhost:24880/ansible/galaxy-operator:main
 
-echo "See https://github.com/pulp/pulp-operator/commit/d4117698d46539e8073040a395d8d4a42c2cc1d4 for context if the following curl fails."
-curl -H "Authorization:Token $TOKEN" http://localhost:24880/api/galaxy/_ui/v1/execution-environments/repositories/ | jq
+curl -H "Authorization:Token $TOKEN" http://localhost:24880/api/galaxy/v3/plugin/execution-environments/repositories/ | jq
 
 cat >> ansible.cfg << ANSIBLECFG
 [defaults]
